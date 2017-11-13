208f3245c8d60210f67fc799f81fe1ce6d314385
Francesco Mannella <francesco.mannella@gmail.com>
2017-11-13 07:34:47 +0100

    bbo-comments

--- DMp/bbo.py
+++ DMp/bbo.py
@@ -17,11 +17,14 @@ class BBO :
         :param lmb: Float. Temperature of the evaluation softmax
         :param epochs: Integer. Number of iterations
         :param rollout_func: Callable object to produce a rollout
+            signature: (thetas - num_rollouts X num_params ) ->
+                (errs -  num_rollouts X num_timesteps,
+                 rollouts -  num_rollouts X num_timesteps)
         '''
         
         self.sigma = sigma
         self.lmb = lmb
-        num_rollouts = num_rollouts
+        self.num_rollouts = num_rollouts
         self.num_params = num_params
         self.theta = np.zeros(num_params)
         self.Cov = np.eye(num_params, num_params)
@@ -33,33 +36,49 @@ class BBO :
     def sample(self):
         """ Get num_rollouts samples from the current parameters mean
         """
+        # matrix of deviations from the parameters mean
         self.eps = np.random.multivariate_normal(
             np.zeros(self.num_params), 
-            self.Cov * self.sigma, num_rollouts)
+            self.Cov * self.sigma, self.num_rollouts)
     
     def update(self, Sk):
         ''' Update parameters
         
-            :param Sk: array(Float) 
+            :param Sk: array(Float), rollout costs in an iteration 
         '''
-        probs = softmax(Sk, self.lmb).reshape(num_rollouts, 1)
+        # Cost-related probabilities of sampled parameters
+        probs = softmax(Sk, self.lmb).reshape(self.num_rollouts, 1)
+        # update with the weighted average of sampled parameters
         self.theta += np.sum(self.eps * probs, 0)
     
-    def eval(self, costs):    
-        errs = costs**2
-        self.err = np.min(np.mean(errs,1))
-        Sk = np.zeros(num_rollouts)
-        for k in range(num_rollouts):
+    def eval(self, errs):
+        """ evaluate rollouts
+            :param errs: array(float), Matrix containing errors 
+                 at each time-step (columns) of each rollout (rows)
+        """
+        errs = errs**2
+        self.err = np.min(np.mean(errs,1)) # store the mean square error
+        
+        # comute costs
+        Sk = np.zeros(self.num_rollouts)
+        for k in range(self.num_rollouts):
+            # final cost
             Sk[k] = errs[k, -1]
+            # cost-to-go integral
             Sk[k] += np.sum( 
                 errs[k, j:].sum() 
                 for j in range(self.num_params)) 
+            # regularization
             thetak = self.theta + self.eps[k]
             Sk[k] += 0.5 * self.sigma * (thetak).dot(thetak) 
         
         return Sk
         
     def iteration(self, explore = True):
+        """ Run an iteration
+            :param explore: Bool, If the iteration is for training (True) 
+                or test (False)
+        """
         self.sample()
         costs, rollouts = self.rollout_func(self.theta + explore*self.eps)    
         Sk = self.eval(costs)
@@ -67,6 +86,8 @@ class BBO :
         self.epoch += 1
         return rollouts, Sk
     
+#------------------------------------------------------------------------------ 
+
 if __name__ == "__main__":
     
     K = 50
@@ -80,16 +101,17 @@ if __name__ == "__main__":
     bbo_sigma = 0.01
     bbo_lmb = 1.0
     epochs = 50
-    
+
+    # create dmps    
     dmps = [ DMP(n, s, g, stime, dt, sigma) 
                     for k in range(K) ]
     
+    # target trajectory
     x = np.linspace(0, 3*np.pi, stime)
     target = np.sin(x) + x
     target /= target.max()
-    #x = np.linspace(0, 1, stime)
-    #target = x
     
+    # function that call the rollouts within the bbo object
     def rollouts(thetas):
         
         rollouts = []
@@ -104,21 +126,27 @@ if __name__ == "__main__":
             errs.append(err)
         
         return np.vstack(errs), np.vstack(rollouts)
-                           
-    bbo = BBO(n, bbo_sigma, bbo_lmb, K, epochs, rollouts)
+    
+    # the BBO object
+    bbo = BBO(n, K, bbo_sigma, bbo_lmb, epochs, rollouts)
     
     costs = np.zeros(epochs)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     line, = ax.plot(costs)
     for t in range(epochs):
+        # iterate -------------
         rs,_ = bbo.iteration()
+        # ---------------------
         costs[t] = bbo.err
         line.set_ydata(costs)
         ax.relim()
         ax.autoscale_view()
         plt.pause(0.001)
+        
+    # test ----------------------------------
     rollouts,_ = bbo.iteration(explore=False)
+    # ---------------------------------------
     
     fig2 = plt.figure()
     plt.plot(target, lw=2, color="red")
